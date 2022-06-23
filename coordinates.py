84022864cc796b0147ef9a62b6ca2664e37f4904
Arvaaz <98753972+Arvaaz@users.noreply.github.com>
2022-06-23 12:38:02 +0530

    Delete coordinates.py

--- coordinates.py
+++
@@ -1,9 +0,0 @@
-from pygeocoder import Geocoder
-
-address = Geocoder.geocode("1 Rugby Street, Newtown, Wellington 6021, New Zealand")
-print(address.valid_address)
-
-print(address.country)
-
-print(address.coordinates)
-# (-41.3068463, 174.7812157)
